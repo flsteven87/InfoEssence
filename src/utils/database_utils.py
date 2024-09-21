1865938eccee677ea20970f8c6c45262bad326f5
WU PO-CHI <po-chi@HUNGdeMacBook-Pro.local>
2024-09-21 15:13:05 +0800

    remove redundant code

--- src/utils/database_utils.py
+++ src/utils/database_utils.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import joinedload
 from src.database.models import News, ChosenNews  # 假設您有一個 News 模型
 from src.config.settings import DATABASE_URL
-from src.utils.file_utils import get_content_file_path
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
